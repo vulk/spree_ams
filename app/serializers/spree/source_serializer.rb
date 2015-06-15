32a4bbd6ef2fc056227b3403d112b9ebab62f1a3
William Harris <wharris@upscalews.com>
2015-06-15 04:09:04 -0400

    update source serializer to not assume there will be credit card details
    attach to source. i.e. for you paypal as a payment source

--- app/serializers/spree/source_serializer.rb
+++ app/serializers/spree/source_serializer.rb
@@ -2,16 +2,32 @@ module Spree
   class SourceSerializer < ActiveModel::Serializer
     embed :ids, include: true
 
-    attributes :id,
-               :cc_type,
-               :last_digits,
-               :year,
-               :month,
-               :name,
-               :gateway_customer_profile_id,
-               :gateway_payment_profile_id
-
     has_one :payment_method, serializer: PaymentMethodSerializer, root: :payment_methods
 
+    def attributes
+      # https://github.com/rails-api/active_model_serializers/issues/738
+      # https://github.com/rails-api/active_model_serializers/issues/599#issuecomment-61915533
+        data = super
+        attrs = [ :id,
+          :cc_type,
+          :last_digits,
+          :year,
+          :month,
+          :name,
+          :gateway_customer_profile_id,
+          :gateway_payment_profile_id]
+         
+        attrs.each do |attr|
+          if object.methods.include? attr
+            data[attr] = object.send(attr)
+          end
+        end
+        data
+    end
+
+    def payment_method
+      # https://github.com/rails-api/active_model_serializers/issues/599
+      object.payment_method if object.methods.include? :payment_method
+    end
   end
 end
