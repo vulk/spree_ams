027c5008bd29f413d4517603a56c9930e2a7b8fe
William Harris <wharris@upscalews.com>
2015-06-16 13:56:07 -0400

    return one record or null from products taxon thingy

--- app/controllers/spree/api/ams/products_controller.rb
+++ app/controllers/spree/api/ams/products_controller.rb
@@ -9,7 +9,7 @@ module Spree
           scope = super
           if params[:taxon_id]
             taxon = Taxon.friendly.find(params[:taxon_id])
-            scope = scope.joins(:classifications).where("#{Spree::Classification.table_name}.taxon_id" => taxon.self_and_descendants.pluck(:id))
+            scope = scope.joins(:classifications).where("#{Spree::Classification.table_name}.taxon_id" => taxon.self_and_descendants.pluck(:id)).take
           end
           scope
         end
